9cae93d60db7875afd37a33b0b4fde11589a877e
Waleed A. Afifi <waleedafifich@gmail.com>
2021-04-01 11:22:39 +0300

    javascript

---
+++ app.js
@@ -0,0 +1,65 @@
+ // alert(11);
+  // alert('Class 102d27');
+  // alert(true);
+
+  // alert(11**11);
+
+  /*
+  
+  Datatype:
+  - String -> "", ''
+  - Number -> 11, 11.2 , 1231231231231231231231231312123
+  - Boolean -> true, false
+
+  - Array
+  - Object
+
+  Mathematics
+  - plus +
+  - minus -
+  - multiply *
+  - divide /
+  - mode %
+  - power **
+
+  Variable
+  - var varName = 11;
+  */
+
+  var mathOperation = prompt('Please enter a number'); // return String
+
+  //  ==
+  //  <=
+  //  >=
+  
+  //  ===
+  //  !=
+  
+  if(mathOperation === 12) {
+    alert(12);
+  }
+  
+   if(mathOperation < 0) {
+     alert('You input is a negative value');
+   } else if(mathOperation >= 0) {  
+     alert('You input is a positive value');
+   } else {
+     alert('please enter a number');
+   }
+  
+  // alert(mathOperation + 3);
+  // alert(mathOperation - 3);
+  // alert(mathOperation * 3);
+  // alert(mathOperation / 3);
+  // alert(mathOperation % 3);
+  // alert(mathOperation ** 3);
+  
+  document.write(mathOperation ** 3);
+  console.log(mathOperation ** 3);
+  
+  var userName = 'Mohammed';
+  // alert(userName);
+  
+  
+  document.write(userName);
+  
